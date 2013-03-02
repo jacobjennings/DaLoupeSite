d80f8f0109abcc12f114544cb4e549c0b9920573
Jacob Jennings <jacob.r.jennings@gmail.com>
2013-03-02 15:32:38 -0800

    Move daloupesite to root.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daloupesite::Application.config.secret_token = 'cbd4b05d00fae2433ca76c4eb033ff2e4ce8da59cbd0724c3064fffe89da518db2ed73b11b5bd1356c0bfe6bf300777f07b5b355e088dcc32f426f1892376202'
